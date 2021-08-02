536e2032d3173e0ba31fd67ee860d8e6c101465e
janos erdos <erdosj@gmail.com>
2021-08-02 22:00:22 +0200

    rm failing test

--- test/ring/jmx_test.clj
+++ test/ring/jmx_test.clj
@@ -2,11 +2,6 @@
   (:require [clojure.test :refer :all]
             [ring.jmx :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
-
 (defn default-handler [request]
   {:status 200
    :body "asdf"
