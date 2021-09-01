61d68d1a716ecbfdca1c04e2aa5a3de69be68d52
Janos Erdos <erdosj@gmail.com>
2021-09-01 22:11:03 +0200

    fix: test dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/erdos/ring-jmx"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.9.4"]]
   :profiles {:test {:plugins [[lein-ring "0.12.5"]]
                     :dependencies [[org.clojure/java.jmx "1.0.0"]]
                     :ring {:handler ring.jmx-test/app}}}
